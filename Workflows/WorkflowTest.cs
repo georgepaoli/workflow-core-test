4a5e78229e50642851c72a721c1233f30bafbd0b
George Paoli <george.paoli@linearsistemas.com.br>
2018-10-15 14:51:22 -0300

    Test saga compensation

---
+++ Workflows/WorkflowTest.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkflowCore.Interface;
+using WorkflowCore.Models;
+
+namespace workflow_core_test.Workflows
+{
+    public class WorkflowTest : IWorkflow
+    {
+        public string Id => this.GetType().Name;
+        public int Version => 1;
+
+        public void Build(IWorkflowBuilder<object> builder)
+        {
+            builder
+                .StartWith(context =>
+                {
+                    Console.WriteLine("Starting workflow...");
+                    return ExecutionResult.Next();
+                })
+                .Saga(saga => saga
+                    .StartWith<Task1>()
+                        .CompensateWith<UndoTask1>()
+                    .Then<Task2>()
+                        .CompensateWith<UndoTask2>()
+                    .Then<Task3>()
+                        .CompensateWith<UndoTask3>()
+                    .Then<Task4>()
+                        .CompensateWith<UndoTask4>()
+                )
+                .OnError(WorkflowCore.Models.WorkflowErrorHandling.Retry, TimeSpan.FromSeconds(5))
+                .Then(context => Console.WriteLine("End"));
+        }
+
+        public class Task1 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Doing Task 1");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class Task2 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Doing Task 2");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class Task3 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Doing Task 3");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class Task4 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Throw exception in Task 4");
+
+                throw new Exception("Test compensation saga..");
+            }
+        }
+
+        public class UndoTask1 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Undoing Task 1");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class UndoTask2 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Undoing Task 2");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class UndoTask3 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Undoing Task 3");
+                return ExecutionResult.Next();
+            }
+        }
+
+        public class UndoTask4 : StepBody
+        {
+            public override ExecutionResult Run(IStepExecutionContext context)
+            {
+                Console.WriteLine("Undoing Task 4");
+                return ExecutionResult.Next();
+            }
+        }
+    }
+}
---
+++ Workflows/WorkflowsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WorkflowCore.Interface;
+
+namespace workflow_core_test.Workflows
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkflowsController : ControllerBase
+    {
+        private readonly IWorkflowHost wfc;
+
+        public WorkflowsController(IWorkflowHost wfc)
+        {
+            this.wfc = wfc;
+        }
+
+        [HttpGet("start-workflow-test")]
+        public ActionResult<string> StartWorkflowTest()
+        {
+            return this.wfc.StartWorkflow(nameof(Workflows.WorkflowTest)).Result;
+        }
+    }
+}
